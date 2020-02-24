fd528460541042dbf3e10ac23848f83810a37abc
rosseffinger <reffinger17@gmail.com>
2020-02-24 12:40:06 -0500

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class main {
+    public main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
---
+++ src/main/java/pages.java
@@ -0,0 +1,8 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class pages {
+    public pages(String input){
+
+    }
+}
